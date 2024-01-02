8da303fc6190aedee57d072cd1e269027b6be1de
Kirk Noda <6519535+wildnerd@users.noreply.github.com>
2024-01-01 18:02:58 -0800

    Fix PC

--- S001/debugging-perl-with-hdb.pl
+++ S001/debugging-perl-with-hdb.pl
@@ -7,10 +7,12 @@ use LWP::UserAgent;
 use HTTP::Request;
 use JSON qw(to_json);
 
+our $VERSION = 0.01;
+
 my $data = to_json( { fname => 'Foo', lname => 'Bar' } );
 
 my $ua = LWP::UserAgent->new( agent => 'Great Perl Agent' );
-$ua->default_header( "Content-Type" => "application/json" );
+$ua->default_header( 'Content-Type' => 'application/json' );
 my $res;
 if (0) {
     $res = $ua->post( 'https://perlmaven.com/', Content => $data );
@@ -19,10 +21,13 @@ else {
     $ua->ssl_opts( verify_hostname => 0, SSL_verify_mode => 0x00 );
     my $req =
       HTTP::Request->new( POST => 'https://192.168.1.67/api/v1/ingest' );
-    $req->authorization_basic( "camainta", "bea##cons" );
-    $req->content_type("application/json");
+    $req->authorization_basic( 'camainta', 'bea##cons' );
+    $req->content_type('application/json');
     $req->content($data);
     $res = $ua->request($req);
 }
 say $res->status_line;
 say $res->content;
+
+print "test";
+
